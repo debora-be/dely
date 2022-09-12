3effdab76174b62b05d5d93b02e995d604665f8d
debora-be <arobedbee@gmail.com>
2022-09-11 23:25:06 -0300

    migration and schema for 'user' context

--- lib/dely/user.ex
+++ lib/dely/user.ex
@@ -4,6 +4,8 @@ defmodule Dely.User do
 
   @primary_key {:id, :binary_id, autogenerate: true}
 
+  @required_params [:age, :address, :cep, :cpf, :email, :password_hash, :name]
+
   schema "users" do
     field :age, :integer
     field :address, :string
@@ -14,5 +16,18 @@ defmodule Dely.User do
     field :name, :string
 
     timestamps()
+  end
+
+  def changeset(user_params) do
+    %__MODULE__{}
+    |> cast(user_params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:password_hash, min: 6)
+    |> validate_length(:cep, is: 8)
+    |> validate_length(:cpf, is: 11)
+    |> validate_number(:age, greater_than_or_equal_to: 18)
+    |> validate_format(:email, ~r/@/)
+    |> unique_constraint([:cpf])
+    |> unique_constraint([:email])
   end
 end
