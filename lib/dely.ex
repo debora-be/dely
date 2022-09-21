e189b89c22ac343cff02fc60b1efeb3c6ac48968
debora-be <arobedbee@gmail.com>
2022-09-20 21:54:52 -0300

    create: delete_user(id)

--- lib/dely.ex
+++ lib/dely.ex
@@ -7,8 +7,10 @@ defmodule Dely do
   if it comes from the database, an external API or others.
   """
   alias Dely.Users.Create, as: UserCreate
+  alias Dely.Users.Delete, as: UserDelete
   alias Dely.Users.Get, as: UserGet
 
   defdelegate create_user(params), to: UserCreate, as: :call
+  defdelegate delete_user(id), to: UserDelete, as: :call
   defdelegate get_user_by_id(id), to: UserGet, as: :by_id
 end
--- lib/dely_web/controllers/users_controller.ex
+++ lib/dely_web/controllers/users_controller.ex
@@ -14,6 +14,14 @@ defmodule DelyWeb.UsersController do
     end
   end
 
+  def delete(conn, %{"id" => id}) do
+    with {:ok, %User{}} <- Dely.delete_user(id) do
+      conn
+      |> put_status(:no_content)
+      |> text("")
+    end
+  end
+
   def show(conn, %{"id" => id}) do
     with {:ok, %User{} = user} <- Dely.get_user_by_id(id) do
       conn
